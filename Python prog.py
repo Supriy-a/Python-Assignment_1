b062a47f63da27f37f2af27ec2da2251877c75d8
Supriy-a <84368495+Supriy-a@users.noreply.github.com>
2021-12-26 15:26:58 +0530

    Add files via upload

---
+++ Python prog.py
@@ -0,0 +1,27 @@
+#1. 
+print("Hello Python")
+
+#store two numbers 2.
+
+num1=input("Enter first number :")
+num2=input("Enter second number :")
+
+#Add two numbers
+sum=float(num1)+float(num2)
+print(sum)
+
+#Substract two numbers
+num1=input("Enter first number :")
+num2=input("Enter second number :")
+
+
+min=float(num1)-float(num2)
+print(min)
+
+#Multiply two numbers
+
+num1=input("Enter first number :")
+num2=input("Enter second number :")
+
+
+mul=float(num1)*float(num2)
